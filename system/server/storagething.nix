6357d2d75bbe626272945045b319357500f889ee
komico <komico@amogus.ye>
2025-03-04 21:31:17 +0800

    samba no more tests 5

--- system/server/storagething.nix
+++ system/server/storagething.nix
@@ -10,6 +10,8 @@
 
     # I'm not gonna lie, this look like too much effort LMAO
     # Just use syncthing or smth like idk man..
+
+    # nvm im just stupid LMAO
     services.samba = {
       enable = true;
       openFirewall = true;
@@ -20,5 +22,7 @@
         comment = "balls.";
       };
     };
+    # why am i adding this to storage? I have no idea.
+    services.logind.lidSwitch = "ignore";
   };
 }
