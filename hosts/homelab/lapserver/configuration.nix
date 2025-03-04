f1dfcb6dd9413da8ee657406fa0e0758c8c6a342
komico <komico@amogus.ye>
2025-03-04 18:53:46 +0800

    minecraft should be FOSS frfr

--- hosts/homelab/lapserver/configuration.nix
+++ hosts/homelab/lapserver/configuration.nix
@@ -35,6 +35,9 @@
   # Enable automatic login for the user.
   services.getty.autologinUser = "komico";
 
+  # damn you Stallman!!
+  nixpkgs.config.allowUnfree = true;
+
   environment.systemPackages = with pkgs; [
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
