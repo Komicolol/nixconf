f9d6199fbec18723d9bb0d80744c31173fcb2eee
komico <komico@amogus.ye>
2025-03-04 21:25:26 +0800

    samba test 4

--- system/server/storagething.nix
+++ system/server/storagething.nix
@@ -13,30 +13,11 @@
     services.samba = {
       enable = true;
       openFirewall = true;
-      settings = {
-        global = {
-            "workgroup" = "WORKGROUP";
-            "server string" = "smbnix";
-            "netbios name" = "smbnix";
-            "security" = "user";
-            #"use sendfile" = "yes";
-            "min protocol" = "smb2";
-            # note: localhost is the ipv6 localhost ::1
-            "hosts allow" = "192.168.0. 127.0.0.1 localhost";
-            "hosts deny" = "0.0.0.0/0";
-            "guest account" = "nobody";
-            "map to guest" = "bad user";
-          };
-          "smbshare" = {
-            "path" = "/home/komico/smbshare";
-            "browseable" = "yes";
-            "read only" = "no";
-            "guest ok" = "no";
-            "create mask" = "0644";
-            "directory mask" = "0755";
-            "force user" = "username";
-            "force group" = "groupname";
-          };
+
+      shares.smbshare = {
+        path = "/home/komico/smbshare";
+        writable = "true";
+        comment = "balls.";
       };
     };
   };
