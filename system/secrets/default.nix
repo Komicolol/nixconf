313b69279c206b714fcb07096d31cc4c745f70d3
komico <komico@amogus.ye>
2025-03-08 22:06:45 +0800

    im fucking dumb :sob:

--- system/secrets/default.nix
+++ system/secrets/default.nix
@@ -11,7 +11,7 @@
     validateSopsFiles = false;
 
     age = {
-      sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key.pub" ];
+      sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
       keyFile = "/var/lib/sops-nix/key.txt";
       generateKey = true;
     };
