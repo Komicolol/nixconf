5c42d4c30b670e46f993e9280f09e41af66bf0cc
komico <komico@amogus.ye>
2025-03-22 23:05:43 +0800

    you have gyatt to be kidding me

--- flake.nix
+++ flake.nix
@@ -68,6 +68,7 @@
     nixOnDroidConfigurations = {
       default = nix-on-droid.lib.nixOnDroidConfiguration {
         pkgs = import nixpkgs { system = "aarch64-linux"; };
+        extraSpecialArgs = { inherit inputs; };
         modules = [
           ./hosts/android/nix-on-droid.nix
         ];
